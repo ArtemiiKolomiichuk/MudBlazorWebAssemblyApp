b8f67ff7a3eb46644dac992e0ee69f65fb8b55da
Artemii Kolomiichuk <95016660+ArtemiiKolomiichuk@users.noreply.github.com>
2024-04-11 22:13:10 +0300

    Add project files.

---
+++ MudBlazorWebAssemblyApp/MudBlazorWebAssemblyApp/Program.cs
@@ -0,0 +1,32 @@
+using MudBlazor.Services;
+using MudBlazorWebAssemblyApp.Client.Pages;
+using MudBlazorWebAssemblyApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
