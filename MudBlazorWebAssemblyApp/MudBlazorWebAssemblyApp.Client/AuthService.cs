66ce7286b46649b15683d48c03befa3ca67e521b
Artemii Kolomiichuk <95016660+ArtemiiKolomiichuk@users.noreply.github.com>
2024-04-12 17:15:54 +0300

    add authService

---
+++ MudBlazorWebAssemblyApp/MudBlazorWebAssemblyApp.Client/AuthService.cs
@@ -0,0 +1,91 @@
+﻿namespace MudBlazorWebAssemblyApp.Client
+{
+    using Microsoft.JSInterop;
+    using System.Net.Http.Headers;
+
+    public class LocalStorageService(IJSRuntime jsRuntime)
+    {
+        public readonly IJSRuntime _jsRuntime = jsRuntime;
+
+        public async Task AddItem(string key, string value)
+        {
+            await _jsRuntime.InvokeVoidAsync("localStorage.setItem", key, value);
+        }
+
+        public async Task RemoveItem(string key)
+        {
+            await _jsRuntime.InvokeVoidAsync("localStorage.removeItem", key);
+        }
+
+        public async Task<string> GetItem(string key)
+        {
+            return await _jsRuntime.InvokeAsync<string>("localStorage.getItem", key);
+        }
+    }
+
+    public class AuthService(LocalStorageService _localStorage)
+    {
+        private readonly LocalStorageService localStorage = _localStorage;
+
+        private string token = "";
+        private string userName = "";
+
+        private bool initialized = false;
+        private async void Init()
+        {
+            if (initialized)
+                return;
+            try
+            {
+                token = await localStorage.GetItem("token");
+                userName = await localStorage.GetItem("userName");
+            }
+            catch{ }
+        }
+
+        public HttpClient Client
+        {
+            get
+            {
+                Init();
+                HttpClient http = new HttpClient();
+                http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", token);
+                return http;
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                Init();
+                return userName;
+            }
+        }
+
+        public void LogOut()
+        {
+            initialized = true;
+            _ = localStorage.RemoveItem("token");
+            _ = localStorage.RemoveItem("userName");
+            token = "";
+            userName = "";
+        }
+
+        public async Task<bool> LogIn(string login, string password)
+        {
+            initialized = true;
+            token = await localStorage._jsRuntime.InvokeAsync<string>("window.btoa", $"{login}:{password}");
+            var response = await Client.PostAsync("http://localhost:3000/login", null);
+            if(!response.IsSuccessStatusCode)
+            {
+                token = "";
+                return false;
+            }
+            userName = login;
+            await localStorage.AddItem("token", token);
+            await localStorage.AddItem("userName", userName);
+            return true;
+        }
+    }
+}
--- MudBlazorWebAssemblyApp/MudBlazorWebAssemblyApp.Client/Program.cs
+++ MudBlazorWebAssemblyApp/MudBlazorWebAssemblyApp.Client/Program.cs
@@ -5,5 +5,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddScoped<MudBlazorWebAssemblyApp.Client.LocalStorageService>();
+builder.Services.AddScoped<MudBlazorWebAssemblyApp.Client.AuthService>();
 
 await builder.Build().RunAsync();
